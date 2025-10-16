74c0c4e941473d052de3ded7e6efc5c8cff73ef8
Guyak <guyotantonin2001@gmail.com>
2025-10-16 21:25:26 +0200

    Cleaning code for maintenance
    - Added functions to run the code with a json configuration files depending on the servers that the bot can access
    - Added options in each slash commands to use said configuration
    - added option in stream_announcement command to change name
    - Modified stream_announcement command to still send a message even if the channel is not live

--- dd_discord_bot.py
+++ dd_discord_bot.py
@@ -1,7 +1,7 @@
 from json import load as j_load
 import discord
 from discord.ext import commands
-from lib_twitch_data_fetcher import get_dict_from_stream
+from lib_twitch_data_fetcher import get_dict_from_stream, StreamNotLiveError
 
 
 
@@ -9,7 +9,33 @@ from lib_twitch_data_fetcher import get_dict_from_stream
 
 bot = commands.Bot()
 
+##————————————————————————————————————————————————————————————————————————————##
 
+config_file = "_config_available_server.json"
+with open(config_file) as file:
+    credentials = j_load(file)
+
+serv_DD = credentials["serv_DD"]
+stream_channel_DD = credentials["stream_channel_DD"]
+in_use_DD = credentials["in_use_DD"]
+serv_Guyal = credentials["serv_Guyal"]
+stream_channel_Guyal = credentials["stream_channel_Guyal"]
+in_use_Guyal = credentials["in_use_Guyal"]
+serv_HR = credentials["serv_HR"]
+stream_channel_HR = credentials["stream_channel_HR"]
+in_use_HR = credentials["in_use_HR"]
+
+def guild_id(use_DD, use_Guyal, use_HR):
+    id = []
+
+    if use_DD and in_use_DD:
+        id.append(serv_DD)
+    if use_Guyal and in_use_Guyal:
+        id.append(serv_Guyal)
+    if use_HR and in_use_HR:
+        id.append(serv_HR)
+    
+    return id
 
 ##————————————————————————————————————————————————————————————————————————————##
 
@@ -21,9 +47,8 @@ bot = commands.Bot()
 @bot.slash_command(
     name="first_slash",
     description="This is the first slash command.",
-    guild_ids=[410481703958740992],
+    guild_ids=guild_id(True, True, False),
 )
-
 async def first_slash(ctx):
     await ctx.respond("You executed the slash command!")
 
@@ -32,7 +57,7 @@ async def first_slash(ctx):
 @bot.slash_command(
     name="second_slash",
     description="This is the second slash command.",
-    guild_ids=[410481703958740992],
+    guild_ids=guild_id(True, True, False),
 )
 async def second_slash(
         ctx, value: str = discord.Option(description="What I will echo")
@@ -44,15 +69,17 @@ async def second_slash(
 @bot.slash_command(
     name="user_info",
     description="Get information about the user executing the command.",
-    guild_ids=[410481703958740992],
+    guild_ids=guild_id(True, True, False),
 )
 async def user_info(ctx):
     user_id = ctx.author.id     # User ID
     username = ctx.author.name  # User name
     nickname = ctx.author.nick  # User nickname (None if not set)
+    guild = ctx.guild.id
 
     output = f"User ID: {user_id} \nUsername: {username} \n\
-        Nickname: {nickname or 'No nickname set'}"
+        Nickname: {nickname or 'No nickname set'} \n\
+        Guild ID: {guild}"
 
     await ctx.respond(output)
 
@@ -61,36 +88,61 @@ async def user_info(ctx):
 @bot.slash_command(
     name="stream_announcement",
     description="Annoncer un stream.",
-    guild_ids=[410481703958740992, 722564006463799357],
+    guild_ids=guild_id(True, True, True),
 )
 async def stream_announcement(
         ctx,
-        pseudo_twitch: str = discord.Option(description=
-                                            "Le pseudonyme du compte Twitch."
+        chaine: str = discord.Option(description=
+                                            "Le nom de la chaine Twitch - Par défaut : harmoniamk",
+                                    default=
+                                            "harmoniamk"
+                                            ),
+        pseudo: str = discord.Option(description=
+                                            "Optionnel - Le pseudo à afficher dans le message",
+                                    required=False
                                             )
     ):
     await ctx.defer()
 
     config_file = "_config_twitch_api.json"
-    channel = bot.get_channel(722564006463799362) # Send the message to #annonces
+    if ctx.guild.id == serv_DD: # If serv DD
+        channel = bot.get_channel(stream_channel_DD) # Send the message to #general
+    if ctx.guild.id == serv_Guyal: # If serv Guyal
+        channel = bot.get_channel(stream_channel_Guyal) # Send the message to #general
+    if ctx.guild.id == serv_HR:    # If serv HR
+        channel = bot.get_channel(stream_channel_HR) # Send the message to #annonces
 
-    user_nickname = ctx.author.nick
+    user_nickname = pseudo or ctx.author.nick
     user_username = ctx.author.name # Use the username if the nickname is empty
 
-    info_dict = get_dict_from_stream(pseudo_twitch, config_file)
-    title = info_dict["stream_title"]
-    game = info_dict["game_name"]
+    url = f"https://www.twitch.tv/{chaine}" # Generate the channel URL
 
-    url = f"https://www.twitch.tv/{pseudo_twitch}"
+    dict_error = False
+    try :
+        info_dict = get_dict_from_stream(chaine, config_file)
+    except StreamNotLiveError:
+        dict_error = True
 
-    output = f"""
-***EN LIVE***
+    if dict_error:
+        output = f"""
+        ***EN LIVE***
+**{user_nickname or user_username}** est très bientôt en stream !
+{url}
+    """
+        await channel.send(output)
+        await ctx.respond("Annonce envoyée ! (*version __stream pas encore lancé__*)", ephemeral=True)
+    else:
+        title = info_dict["stream_title"]
+        game = info_dict["game_name"]
+
+        output = f"""
+        ***EN LIVE***
 **{user_nickname or user_username}** est en stream sur __{game}__ !
 **{title}**
 {url}
-    """
-    await channel.send(output)
-    await ctx.respond("Annonce envoyée !", ephemeral=True)
+        """
+        await channel.send(output)
+        await ctx.respond("Annonce envoyée !", ephemeral=True)
 
 
 
